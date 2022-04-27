e7273f86958b2710ee89687d9c1f2ba89c271d88
yuanxiaowei <yxw159@163.com>
2022-04-27 22:48:53 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-27 15:17:20
+ * @LastEditTime: 2022-04-27 15:37:46
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \cloud-music\src\App.js
+ */
+import { IconStyle } from './assets/iconfont/iconfont'
+import { GlobalStyle } from './style'
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle></GlobalStyle>
+      <IconStyle></IconStyle>
+      yxw
+      <i className="iconfont">&#xe62b;</i>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-27 15:17:20
+ * @LastEditTime: 2022-04-27 15:24:23
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \cloud-music\src\index.js
+ */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
